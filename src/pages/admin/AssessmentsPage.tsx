3e537a4ea510136be1cff2e0b3e0258f99741f61
prashob-earlyjobs <prashob@earlyjobs.in>
2025-07-30 23:37:41 +0530

    Update AssessmentsPage.tsx

--- src/pages/admin/AssessmentsPage.tsx
+++ src/pages/admin/AssessmentsPage.tsx
@@ -244,7 +244,7 @@ const AssessmentsPage: React.FC = () => {
                 <div className="relative">
                   <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
                   <Input
-                    placeholder="Search s..."
+                    placeholder="Search assessments..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     className="pl-10 w-64"
@@ -254,8 +254,8 @@ const AssessmentsPage: React.FC = () => {
             </CardHeader>
             <CardContent>
               <div className="space-y-4">
-                {s.map((, index) => {
-                  const isLast = index === s.length - 1;
+                {assessments.map((assessment, index) => {
+                  const isLast = index === assessments.length - 1;
                   return (
                     <div
                       key={assessment._id}
